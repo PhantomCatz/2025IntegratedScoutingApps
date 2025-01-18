e39af2b92ac51556bfb311f942dec4bd06e84ec9
raileychen <railey.ch@gmail.com>
2025-01-18 13:56:19 -0800

    updated DTF form

--- integrated-scouting-apps/src/routes/dtf.tsx
+++ integrated-scouting-apps/src/routes/dtf.tsx
@@ -14,53 +14,47 @@ function DTF(props: any) {
 
 	return (
 		<div>
-			<meta name="viewport" content="maximum-scale=1.0" />
-			<div className='banner'>
-				<header>
-					<a href='/home'>
-						<img src={back} style={{ height: 64 + 'px', paddingTop: '5%' }} alt=''></img>
-					</a>
-					<table>
-						<tbody>
-							<tr>
-								<td>
-									<img src={logo} style={{ height: 256 + 'px' }} alt=''></img>
-								</td>
-								<td>
-									<h1 style={{ display: 'inline-block', textAlign: 'center' }}>Drive Team Feeder</h1>
-								</td>
-							</tr>
-
-						</tbody>
-
-					</table>
-				</header>
-			</div>
+		  <div className='banner'>
+			<header>
+		   <a href='/home'>
+					<img src={back} style={{ height: 64 + 'px', paddingTop: '5%' }} alt=''></img>
+				</a>
+			<img src={logo} style={{ height: '256px' }} alt='Logo' />
+			<h1 style={{ display: 'inline-block', textAlign: 'center' }}>Drive Team Feeder</h1>
+			</header>
+		  </div>
+			
 			<Form
-				form={form}
-				onFinish={async event => {
-					const teamNums = [event.team1Num, event.team2Num, event.team3Num].filter(num => num !== undefined);
-					window.location.href = "/dtf/" + teamNums.join(",");
-				}}
+			  form={form}
+			  onFinish={async event => {
+				const teamNums = [event.team1Num, event.team2Num, event.team3Num].filter(num => num !== undefined);
+				window.location.href = "/dtf/" + teamNums.join(",");
+			  }}
 			>
-				<div>
-					<h2>Team 1 Number</h2>
-					<Form.Item name="team1Num" rules={[{ required: true, message: "Please input the team number!" }]}>
-						<InputNumber min={0} className="input" />
-					</Form.Item>
-					<h2>Team 2 Number</h2>
-					<Form.Item name="team2Num">
-						<InputNumber min={0} className="input" />
-					</Form.Item>
-					<h2>Team 3 Number</h2>
-					<Form.Item name="team3Num">
-						<InputNumber min={0} className="input" />
-					</Form.Item>
-					<Input type="submit" value="Submit" className='submit' />
-				</div>
+			  <div>
+				<h2>Team 1 Number</h2>
+				<Form.Item name="team1Num" rules={[{ required: true, message: "Please input the team number!" }]} >
+				  <InputNumber min={0} className="input" />
+				</Form.Item>
+	  
+				<h2>Team 2 Number</h2>
+				<Form.Item name="team2Num">
+				  <InputNumber min={0} className="input" />
+				</Form.Item>
+				
+				<h2>Team 3 Number</h2>
+				<Form.Item name="team3Num">
+				  <InputNumber min={0} className="input" />
+				</Form.Item>
+	  
+				<Form.Item>
+				  <Input type="submit" value="Submit" className="submit" />
+				</Form.Item>
+			  </div>
 			</Form>
-		</div>
-	);
-}
-
-export default DTF;
+		  </div>
+		);
+	  };
+	  
+	  export default DTF;
+	  
