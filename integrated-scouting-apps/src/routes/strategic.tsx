6c84f0c94248d0d98cea301bf1e1024ba79a642f
Strawblblberry <lilliesandlily2007@gmail.com>
2025-02-02 18:46:46 -0800

    made it blue, basically. also removed times amp

--- integrated-scouting-apps/src/routes/strategic.tsx
+++ integrated-scouting-apps/src/routes/strategic.tsx
@@ -2,14 +2,14 @@ import '../public/stylesheets/style.css';
 import '../public/stylesheets/strategic.css';
 import logo from '../public/images/logo.png';
 import back from '../public/images/back.png';
-import { useEffect, useState } from 'react';
+import { useEffect, useState} from 'react';
 import { Tabs, Input, Form, Select, InputNumber, Button, Flex } from 'antd';
 import TextArea from 'antd/es/input/TextArea';
 import VerifyLogin from '../verifyToken';
 import { useCookies } from 'react-cookie';
 import { saveAs } from 'file-saver';
 
-function Strategic(props: any) {
+function Strategic(props: any, text:any) {
   const [form] = Form.useForm();
   const [tabNum, setTabNum] = useState("1");
   const [teamNum, setTeamNum] = useState(0);
@@ -162,8 +162,8 @@ function Strategic(props: any) {
           <Select options={rounds} className="input" onChange={() => { calculateMatchLevel(); updateTeamNumber(); }} />
         </Form.Item>
         <h2>Match #</h2>
-        <Form.Item<FieldType> name="matchnum" rules={[{ required: true, message: 'Please input the match number!' }]}>
-          <InputNumber min={1} className="input" onChange={() => { updateTeamNumber(); }} />
+        <Form.Item<FieldType> name="matchnum" rules={[{ required: true, message: 'Please input the match number!',  }]}>
+          <InputNumber min={1} className = "input" onChange={() => { updateTeamNumber(); }} type='number' /> 
         </Form.Item>
         <h2 style={{ display: roundIsVisible ? 'inherit' : 'none' }}>Round #</h2>
         <Form.Item<FieldType> name="roundnum" rules={[{ required: roundIsVisible ? true : false, message: 'Please input the round number!' }]} style={{ display: roundIsVisible ? 'inherit' : 'none' }}>
@@ -171,28 +171,26 @@ function Strategic(props: any) {
         </Form.Item>
         <h2>Robot Position</h2>
         <Form.Item<FieldType> name="robotpos" rules={[{ required: true, message: 'Please input the robot position!' }]}>
-          <Select options={robotpos} onChange={() => { updateTeamNumber(); }} className="input" />
+          <Select options={robotpos} onChange={() => { updateTeamNumber(); }} className='input' listItemHeight={10} listHeight={500} placement='bottomLeft'/>
         </Form.Item>
         <Flex justify='in-between' style={{ paddingBottom : '5%' }}>
           <Button onClick={() => setTabNum("2")} className='tabbutton'>Next</Button>
         </Flex>
       </div>
     );
-  }
+  } 
+
+  
   function comment() {
     type FieldType = {
       comments: string;
-      timesamplified: number;
+    
     };
     return (
       <div>
         <h2>Comments</h2>
         <Form.Item<FieldType> name="comments" rules={[{ required: true, message: "Please input some comments!" }]}>
           <TextArea style={{ verticalAlign: 'center' }} className='strategic-input' />
-        </Form.Item>
-        <h2>Times Amplified</h2>
-        <Form.Item<FieldType> name="timesamplified" rules={[{ required: true, message: 'Please input the number of times the speaker was amplified!' }]}>
-          <InputNumber type='number' pattern="\d*" onWheel={(event) => (event.target as HTMLInputElement).blur()} min={0} className="input" />
         </Form.Item>
         <h2 style={{ display: isLoading ? 'inherit' : 'none' }}>Submitting data...</h2>
         <Flex justify='in-between' style={{ paddingBottom : '5%' }}>
@@ -270,6 +268,6 @@ function Strategic(props: any) {
       </Form>
     </div>
   );
-}
+} 
 
-export default Strategic;
+export default Strategic; 
