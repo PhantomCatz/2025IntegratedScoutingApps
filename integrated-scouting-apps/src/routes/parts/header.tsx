49f2b90db50009fc1a14573e3fb1eb9a47a97857
brianyoshimitsu <byreevee@gmail.com>
2025-03-22 16:16:10 -0700

    Header Changes

--- integrated-scouting-apps/src/routes/parts/header.tsx
+++ integrated-scouting-apps/src/routes/parts/header.tsx
@@ -1,7 +1,7 @@
-import back from '../../public/images/back.png';
-import darkback from '../../public/images/dark_back.png';
-import logo from '../../public/images/logo.png';
-import darklogo from '../../public/images/dark_logo.png';
+import lightBack from '../../public/images/back.png';
+import darkBack from '../../public/images/dark_back.png';
+import lightLogo from '../../public/images/logo.png';
+import darkLogo from '../../public/images/dark_logo.png';
 import '../../public/stylesheets/style.css';
 import '../../public/stylesheets/header.css';
 import React, { useState, useEffect } from 'react';
@@ -13,68 +13,76 @@ import React, { useState, useEffect } from 'react';
  * props.name = name of current page
  */
 function Header(props: any) {
-  const isRootPage = props.rootPage || false;
-  const name = props.name || "No name set";
-  const backLink = props.back || "/";
+	const isRootPage = props.rootPage || false;
+	const name = props.name || "No name set";
+	const backLink = props.back || "/";
 
-  const [theme, setTheme] = useState<'light' | 'dark' | 'pink'>('light'); 
-  const root2 = () => {
-    const root = document.documentElement;
-    root.style.setProperty('--pc-blue', ' #ffffff');// #32a7dc
-    root.style.setProperty('--font-color', '#000000');
-  };
+	const [theme, setTheme] = useState<'light' | 'dark' | 'pink'>('dark'); 
 
-  const root3 = () => {
-    const root = document.documentElement;
-    root.style.setProperty('--pc-blue', ' #000000'); 
-    root.style.setProperty('--font-color', '#ffffff');
-  };
-
-  const root4 = () => {
-    const root = document.documentElement;
-    root.style.setProperty('--pc-blue', '#f79ac0'); 
-    root.style.setProperty('--font-color', ' #f1f3ffff');
-  };
-
-  const handleLogoClick = () => {
-	if (theme === 'pink') {
-		setTheme('light');
-	  } else {
-		if (theme === 'light') {
-		  setTheme('dark');
-		} else if (theme === 'dark') {
-		  setTheme('light');
-		}
-	  }
+	const colors = {
+		light: {
+			'--pc-blue': ' #ffffff',// #32a7dc
+			'--font-color': '#000000',
+		},
+		dark: {
+			'--pc-blue': ' #000000',
+			'--font-color': '#ffffff'
+		},
+		pink: {
+			'--pc-blue': '#f79ac0',
+			'--font-color': ' #f1f3ffff',
+		},
+	};
+	const icons = {
+		light: {
+			icon: darkLogo,
+			back: darkBack,
+		},
+		dark: {
+			icon: lightLogo,
+			back: lightBack,
+		},
+		pink: {
+			icon: lightLogo,
+			back: lightBack,
+		},
 	};
 
-  const handleLogoDoubleClick = () => {
-    setTheme('pink');
-  };
+	const handleLogoClick = () => {
+		if (theme === 'pink') {
+			setTheme('light');
+		} else if (theme === 'light') {
+			setTheme('dark');
+		} else if (theme === 'dark') {
+			setTheme('light');
+		}
+	};
 
-  useEffect(() => {
-    if (theme === 'light') {
-      root2();
-    } else if (theme === 'dark') {
-      root3();
-    } else if (theme === 'pink') {
-      root4();
-    }
-  }, [theme]);
+	const handleLogoDoubleClick = () => {
+		setTheme('pink');
+	};
 
-  return (
-    <header className="header">
-      {!isRootPage && <a href={backLink}><img className={"backImg"} src={theme === 'dark' ? back : darkback} alt=''></img></a>}
-      <img
-        className={"logoImg"}
-		src={theme === 'pink' ? logo : theme === 'dark' ? logo : darklogo}
-        onClick={handleLogoClick} 
-        onDoubleClick={handleLogoDoubleClick} 
-        alt="Logo"
-      />
-      <h1 className={"pageTitle"}>{name}</h1>
-    </header>
-  );
+	useEffect(() => {
+		const root = document.documentElement;
+		for(const [variable, color] of Object.entries(colors[theme])) {
+			root.style.setProperty(variable, color); 
+		}
+	}, [theme]);
+
+	return (
+		<header className="header">
+		{!isRootPage &&
+			<a href={backLink}><img className={"backImg"} src={icons[theme].back} alt=''></img></a>}
+			<img
+				className={"logoImg"}
+				src={icons[theme].icon}
+				onClick={handleLogoClick} 
+				onDoubleClick={handleLogoDoubleClick} 
+				alt="Logo"
+			/>
+			<h1 className={"pageTitle"}>{name}</h1>
+		</header>
+	);
 }
 
 export default Header;
