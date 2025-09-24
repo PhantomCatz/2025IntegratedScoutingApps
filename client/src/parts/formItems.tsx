718d892cfee132dfeb398a5a508fb593c9fc729e
brianyoshimitsu <byreevee@gmail.com>
2025-09-24 16:48:58 -0700

    Change number input

--- client/src/parts/formItems.tsx
+++ client/src/parts/formItems.tsx
@@ -15,8 +15,6 @@ type NumberInputType<FieldType> = {
   message?: string;
   min?: number;
   max?: number;
-  onIncrease?: (e?: number) => void;
-  onDecrease?: (e?: number) => void;
   onChange?: (e? : number) => void;
   align?: AlignOptions;
   shown?: boolean;
@@ -46,166 +44,76 @@ function NumberInput<FieldType>(props: NumberInputType<NoInfer<FieldType>>) {
   const message = props.message ?? `Please input ${title}`;
   const min = props.min ?? 0;
   const max = props.max ?? Infinity;
-  const onIncrease = props.onIncrease ?? (() => {});
-  const onDecrease = props.onDecrease ?? (() => {});
   const onChange = props.onChange ?? (() => {});
   const align = props.align ?? "center";
   const buttons = props.buttons ?? true;
 
-  const updateValue = (f : ((event : any) => void)) => {
-    const oldVal = form.getFieldValue(name);
+  const input = useRef(null);
 
-    const newVal = f(oldVal);
-
-    form.setFieldValue(name, newVal);
-  };
-
-  const [value, setValue] = useState<number>(min);
-  const [prevValue, setPrevValue] = useState<number>(min);
-
-  useEffect(() => {
-    if(prevValue < value) {
-      onIncrease(value);
-    } else if(prevValue > value) {
-      onDecrease(value);
+  function updateInputValue(delta) {
+    const newVal = (parseInt(input.current.value) || 0) + delta;
+    if(newVal > max) {
+      input.current.value = max;
+    } else if(newVal < min) {
+      input.current.value = min;
+    } else {
+      input.current.value = newVal;
     }
-
-    form.setFieldValue(name, value);
-
-    onChange(value);
-    setPrevValue(value);
-  }, [value]);
-
-  const handleChange : ((event: any) => void) = (() => {
-    let id : any = null;
-
-    return function(event : any) {
-      window.clearTimeout(id);
-
-      id = setTimeout(async () => {
-        const newValue : string = event.nativeEvent.target.value;
-        const num : number = toNumber(newValue);
-
-        if(!num) {
-          return;
-        }
-
-        let updatedNumber = 0;
-
-        await updateValue((prevForm : any) => {
-          const newVal : string = num.toString();
-
-          const newNumber : number = Math.max(Math.min(toNumber(newVal), max), min);
-
-          updatedNumber = newNumber;
-
-          return updatedNumber;
-        });
-
-        setValue(updatedNumber);
-
-      }, 50);
-    }
-  })();
+  }
+  async function handleChange(e) {
+    const newVal = e?.target?.value;
+    await onChange(newVal);
+  }
 
   return (
-    <Flex
-      vertical
-      align='flex-start'
-      style={{
-        display: shown ? 'inherit' : 'none',
-      }}
-      className={"numberInput"}
-    >
-      {title &&
-        <h2
+    <>
+      {shown &&
+        <div
+          className="input input__number"
           style={{
-            textAlign: align as any,
-            width: "100%",
+            align: align,
           }}
-        >{title}</h2>
+        >
+          {title &&
+            <label
+              style={{
+                textAlign: align,
+              }}
+              htmlFor={name}
+            >{title}</label>
+          }
+          <div>
+            { buttons &&
+              <button
+                type="button"
+                className="changeButton changeButton__decrement"
+                onClick={ () => {
+                  updateInputValue(-1);
+                }}
+              >-</button>
+            }
+            <input
+              id={name}
+              ref={input}
+              type="number"
+              className=""
+              min={min}
+              max={max}
+              onChange={handleChange}
+            />
+            { buttons &&
+              <button
+                type="button"
+                className="changeButton changeButton__increment"
+                onClick={ () => {
+                  updateInputValue(1);
+                }}
+              >+</button>
+            }
+          </div>
+        </div>
       }
-      <Form.Item<FieldType>
-        name={name as any}
-        rules={[
-          {
-            required: required,
-            message: message,
-          },
-          () => ({
-            async validator(rule : any, value, callback) {
-              if(required) {
-                if(value < min) {
-                  return Promise.reject(`${title} must be at least ${min}`);
-                }
-                if(value > max) {
-                  return Promise.reject(`${title} must be at most ${max}`);
-                }
-              }
-
-              return Promise.resolve();
-            }
-          })
-        ]}
-      >
-        <Input
-          id={name}
-          type={"text"}
-          inputMode={"numeric"}
-          className={"input"}
-          onKeyDown={(e) => {
-            const key = e.keyCode;
-            const acceptedKeys = [37, 38, 39, 40, 224];
-            if((key >= 32 && !acceptedKeys.includes(key)) && ( key < ord("0") || key > ord("9"))) {
-              console.log("prevented", key);
-              e.nativeEvent.preventDefault();
-            }
-          }}
-          onWheel={(e) => {
-            (e.target as HTMLElement).blur();
-          }}
-          onKeyUp={handleChange}
-          {...(buttons ? {
-            addonBefore: (
-              <Button
-                className={"changeButton changeButton__decrement"}
-                onMouseDown={async (form: any) => {
-                  let updatedNumber = 0;
-
-                  await updateValue((prevVal: any) => {
-                    const val = toNumber(prevVal) - 1;
-
-                    updatedNumber= Math.max(val, min);
-
-                    return updatedNumber;
-                  });
-
-                  await setValue(updatedNumber);
-                }}
-              >-</Button>
-            ),
-            addonAfter: (
-              <Button
-                className={"changeButton changeButton__increment"}
-                onMouseDown={async () => {
-                  let updatedNumber = 0;
-
-                  await updateValue((prevVal : any) => {
-                    const val = toNumber(prevVal) + 1;
-
-                    updatedNumber = Math.min(val, max);
-
-                    return updatedNumber;
-                  });
-
-                  await setValue(updatedNumber);
-                }}
-              >+</Button>
-            ),
-          } : {})}
-        />
-      </Form.Item>
-    </Flex>
+    </>
   );
 }
 function Select<FieldType>(props: SelectType<FieldType>) {
@@ -230,11 +138,12 @@ function Select<FieldType>(props: SelectType<FieldType>) {
       }}
     >
       {title &&
-        <h2
+        <label
           style={{
             textAlign: align as any,
           }}
-        >{title}</h2>
+          htmlFor={name}
+        >{title}</label>
       }
       <Form.Item<FieldType>
         name={name as any}
@@ -243,7 +152,6 @@ function Select<FieldType>(props: SelectType<FieldType>) {
         <AntdSelect
           options={options}
           onChange={onChange}
-          className="input"
           dropdownMatchSelectWidth={false}
           dropdownStyle={{ maxHeight: 'none' }}
           mode={multiple}
