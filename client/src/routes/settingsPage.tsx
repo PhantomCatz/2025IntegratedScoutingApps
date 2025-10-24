6a09f71dd53abdca81da823e79abc0131682f843
brianyoshimitsu <byreevee@gmail.com>
2025-10-23 18:34:09 -0700

    feat: allow users to download tba data

--- client/src/routes/settingsPage.tsx
+++ client/src/routes/settingsPage.tsx
@@ -10,115 +10,153 @@ import { request } from '../utils/tbaRequest';
 const DEFAULT_SETTINGS = {
   backgroundColor: '#ffffff',
   fontColor: '#000000',
-	scouterIntial:'',
-	eventCode: EVENT_NAME ,
-	tbaData:{},
-	tbaTeams:{},
+  scouterIntial: '',
+  eventCode: EVENT_NAME ,
+  tbaData: {},
+  tbaTeams: {},
+  updateTimes: {},
 } as const;
 
 function Settings(props: any) {
-  const [background, setBackground] = useLocalStorage<any>('background', DEFAULT_SETTINGS.backgroundColor);
+  const [backgroundColor, setBackgroundColor] = useLocalStorage<any>('backgroundColor', DEFAULT_SETTINGS.backgroundColor);
   const [fontColor, setFontColor] = useLocalStorage<any>('fontColor', DEFAULT_SETTINGS.fontColor);
-	const [scouterIntial, setScouterIntial] = useLocalStorage<any>('scouterIntial', DEFAULT_SETTINGS.scouterIntial);
-	const [eventCode, setEventCode] = useLocalStorage<any>('eventCode', DEFAULT_SETTINGS.eventCode);
-	const [tbaData, setTbaData] = useLocalStorage<any>('tbaData', DEFAULT_SETTINGS.tbaData);
-	const [tbaTeams, setTbaTeams] = useLocalStorage<any>('tbaTeams', DEFAULT_SETTINGS.tbaTeams);
-	const textColorInput = useRef(null)
-	const backgroundColorInput = useRef(null)
-	//const [theme] = useLocalStorage<any>('theme');
-	//onChange={function(event) {setFontColor(color["--background-color"]);}}
-	const rootElement = document.querySelector(":root") as any;
-			rootElement.style.setProperty('--background-color', background);
-			rootElement.style.setProperty('--font-color', fontColor);
+  const [scouterIntial, setScouterIntial] = useLocalStorage<any>('scouterIntial', DEFAULT_SETTINGS.scouterIntial);
+  const [eventCode, setEventCode] = useLocalStorage<any>('eventCode', DEFAULT_SETTINGS.eventCode);
+  const [tbaData, setTbaData] = useLocalStorage<any>('tbaData', DEFAULT_SETTINGS.tbaData);
+  const [tbaTeams, setTbaTeams] = useLocalStorage<any>('tbaTeams', DEFAULT_SETTINGS.tbaTeams);
+  const [updateTimes, setUpdateTimes] = useLocalStorage<any>('updateTimes', DEFAULT_SETTINGS.updateTimes);
 
-	async function updateTeams (matchEvent : string) {
-		try {
-			const response = await request('event/' + matchEvent + '/teams');
-			const teams = await response.json();
+  const textColorInput = useRef(null)
+  const backgroundColorInput = useRef(null)
 
-			const numbers = teams.map((x : any) => x.team_number);
+  const rootElement = document.querySelector(":root") as any;
 
-			numbers.sort((a : any, b : any) => a - b);
+  useEffect(() => {
+    rootElement.style.setProperty('--background-color', backgroundColor);
+    rootElement.style.setProperty('--font-color', fontColor);
+  }, [backgroundColor, fontColor]);
 
-			tbaTeams[matchEvent] = numbers;
-		} catch(err) {
-			window.alert("An error occurred:\n" + err);
-		}
-	}
+  async function updateTeams (matchEvent : string) {
+    const response = await request('event/' + matchEvent + '/teams');
+    const teams = await response.json();
 
-	async function updateData(matchEvent: string) {
-		try {
-			const response = await request(`event/${matchEvent}/matches`);
-			const data = await response.json();
-			const newData = [];
-			data.forEach((row) => {
-			const fullTeams: string[] = [];
-				for(const color of ["red", "blue"]) {
-					data.alliances[color].team_keys.forEach((team : any) => fullTeams.push(team.substring(3)));
-				}
-				newData.push(fullTeams);
-		});
+    const numbers = teams.map((x : any) => x.team_number);
 
-    setTbaData(newData);
+    numbers.sort((a : any, b : any) => a - b);
 
+    tbaTeams[matchEvent] = numbers;
+  }
 
-			console.log(data);
-			data.map((row) => {
-				const fullTeams: string[] = [];
-				for(const color of ["red", "blue"]) {
-					row.alliances[color].team_keys.forEach((team : any) => fullTeams.push(team.substring(3)));
-				}
-				return fullTeams;
-			});
-			setTbaData(data);
-		} catch(err) {
-			window.alert("An error occurred:\n" + err);
-		}
-	}
+  async function updateData(matchEvent: string) {
+    const response = await request(`event/${matchEvent}/matches`);
+    const data = await response.json();
+
+    const newData = data.map((row) => {
+      const fullTeams: string[] = [];
+      for(const color of ["red", "blue"]) {
+        row.alliances[color].team_keys.forEach((team : any) => fullTeams.push(team.substring(3)));
+      }
+      return fullTeams;
+    });
+
+    const newTbaData = { ...tbaData };
+
+    newTbaData[matchEvent] = newData;
+
+    setTbaData(newTbaData);
+  }
+
+  const events = [];
+  let index = 0;
+  for(const [eventCode, time] of Object.entries(updateTimes)) {
+    events.push(
+      <div className="updateTime" key={`updateTime${index++}`}>
+        <h1 className="updateTime__title">{eventCode}:</h1>
+        <p className="updateTime__timestamp">{time}</p>
+      </div>
+    );
+  }
 
 
   return (
-    <>
+    <div className="settingsPage">
       <Header name={"Settings"} back="#/" />
 
-			<h1>Scouter Initials</h1>
-          <Input
-            maxLength={2}
-            className="input"
-						onChange={(event) => setScouterIntial(event.target.value)}
-            onKeyPress={(event) => {
-              const keyCode = event.keyCode || event.which;
-              const keyValue = String.fromCharCode(keyCode);
-              if (!/^[A-Za-z]*$/.test(keyValue)) {
-                event.preventDefault();
-              }
-            }}
-						value={scouterIntial}
-          />
+      <h1>Scouter Initials</h1>
+      <Input
+        maxLength={2}
+        className="input"
+        onChange={(event) => setScouterIntial(event.target.value)}
+        onKeyPress={(event) => {
+          const keyCode = event.keyCode || event.which;
+          const keyValue = String.fromCharCode(keyCode);
+          if (!/^[A-Za-z]*$/.test(keyValue)) {
+            event.preventDefault();
+          }
+        }}
+        value={scouterIntial}
+      />
 
-			<h1>Event Code</h1>
-          <Input
-            className="input"
-						onChange={(event) => setEventCode(event.target.value)}
-						value={eventCode}
-          />
-			<button onClick={function(event) {
-				updateTeams(eventCode);
-				updateData(eventCode); }}
-				className='fetchDataButton'> Fetch Data </button>
+      <h1>Event Code</h1>
+      <Input
+        className="input"
+        onChange={(event) => setEventCode(event.target.value)}
+        value={eventCode}
+      />
+      <button
+        onClick={async function(event) {
+          try {
+            await updateTeams(eventCode);
+            await updateData(eventCode);
+            const newUpdateTimes = { ...updateTimes };
+            newUpdateTimes[eventCode] = Date();
 
-			 <h1>Color Theme</h1>
-			<div>
-				<input ref ={textColorInput} onChange={(event) => setFontColor(event.target.value)} type="color" id="textcontrast" name="textcontrast" defaultValue={fontColor}/>
-				<label htmlFor="textcontrast"> Text color </label>
-			</div>
+            setUpdateTimes(newUpdateTimes);
+          } catch(err) {
+            window.alert(`An error occurred: `, err);
+          }
+        }}
+        className='fetchDataButton'
+        type="button"
+      >Fetch Data</button>
+      <button
+        onClick={function(event) {
+          setTbaTeams({});
+          setTbaData({});
+          setUpdateTimes({});
+        }}
+        className="clearDataButton"
+        type="button"
+      >Clear Data</button>
 
-			<div>
-			<input ref={backgroundColorInput} onChange={(event) => setBackground(event.target.value)} type="color" id="background" name="background" defaultValue={background}/>
-			<label htmlFor="background"> Background color</label>
-		</div>
+      {...events}
 
-    </>
+      <h1>Color Theme</h1>
+      <div className="input__color">
+        <input
+          ref={textColorInput}
+          onChange={(event) => setFontColor(event.target.value)}
+          type="color"
+          id="textColor"
+          name="textColor"
+          defaultValue={fontColor}
+        />
+        <label htmlFor="textColor">Text color</label>
+      </div>
+
+      <div className="input__color">
+        <input
+          ref={backgroundColorInput}
+          onChange={(event) => setBackgroundColor(event.target.value)}
+          type="color"
+          id="backgroundColor"
+          name="backgroundColor"
+          defaultValue={backgroundColor}
+        />
+        <label htmlFor="backgroundColor">Background color</label>
+      </div>
+
+    </div>
   );
 }
 
